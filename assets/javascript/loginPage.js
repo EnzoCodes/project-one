805c9a3dcdb1925d4bd55ef1a518df924c8cf42b
Your Name <enzocnop@gmail.com>
2017-11-10 17:49:07 -0800

    nearly working v1

--- assets/javascript/loginPage.js
+++ assets/javascript/loginPage.js
@@ -1,32 +1,47 @@
-// Initialize Firebase
-var config = {
-apiKey: "AIzaSyAKovIgnElTXfZog6-eGf7X3vU1I7go6yI",
-authDomain: "imagewordmatch.firebaseapp.com",
-databaseURL: "https://imagewordmatch.firebaseio.com",
-projectId: "imagewordmatch",
-storageBucket: "imagewordmatch.appspot.com",
-messagingSenderId: "621229379920"
-};
-firebase.initializeApp(config);
+(function () {
+    // Initialize Firebase
+    var config = {
+    apiKey: "AIzaSyAKovIgnElTXfZog6-eGf7X3vU1I7go6yI",
+    authDomain: "imagewordmatch.firebaseapp.com",
+    databaseURL: "https://imagewordmatch.firebaseio.com",
+    projectId: "imagewordmatch",
+    storageBucket: "imagewordmatch.appspot.com",
+    messagingSenderId: "621229379920"
+    };
+    firebase.initializeApp(config);
 
-var provider = new firebase.auth.GoogleAuthProvider();
 
-function signIn() {
-    firebase.auth().signInWithPopup(provider).then(function(result) {
-    // This gives you a Google Access Token. You can use it to access the Google API.
-    var token = result.credential.accessToken;
-    // The signed-in user info.
-    var user = result.user;
-    console.log(user.displayName);
-    // ...
-    }).catch(function(error) {
-    // Handle Errors here.
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    // The email of the user's account used.
-    var email = error.email;
-    // The firebase.auth.AuthCredential type that was used.
-    var credential = error.credential;
-    // ...
+    const txtEmail = document.getElementById('txtEmail');
+    const txtPassword = document.getElementById('txtPassword');
+    const btnLogin = document.getElementById('btnLogin');
+    const btnSignUp = document.getElementById('btnSignUp');
+    const btnLogout = document.getElementById('btnLogout');
+
+    //Login Event
+    btnLogin.addEventListener('click', e => {
+        //Grabbing Email/Pass from form
+        const email = txtEmail.value;
+        const pass = txtPassword.value;
+        const auth = firebase.auth();
+        // Sign In
+        const promise = auth.signInWithEmailAndPassword(email, pass);
+        promise.catch(e => console.log(e.message));
     });
-}
+
+    btnLogin.addEventListener('click', e => {
+        const email = txtEmail.value;
+        const pass = txtPassword.value;
+        const auth = firebase.auth();
+        // Sign In
+        const promise = auth.createUserWithEmailAndPassword(email, pass);
+        promise.catch(e => console.log(e.message));
+
+    });
+//
+// auth.signInWithEmailAndPassword(email, pass);
+//
+// auth.createUserWithEmailAndPassword(email, pass);
+//
+// auth.onAuthStateChanged(firebaseUser => { });
+
+}());
