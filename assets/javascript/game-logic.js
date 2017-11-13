4e7cea80a54ca5003b6e0ffae8caebac683b2d6a
Diana <dianaltecson@gmail.com>
2017-11-12 17:40:54 -0800

    added firebase skeleton for multiplayer functionality

--- assets/javascript/game-logic.js
+++ assets/javascript/game-logic.js
@@ -184,6 +184,105 @@ $('#generate-photo-info').click(function () {
 })
 
 
+// ============================================================================
+
+var config = {
+    apiKey: "AIzaSyAKovIgnElTXfZog6-eGf7X3vU1I7go6yI",
+    authDomain: "imagewordmatch.firebaseapp.com",
+    databaseURL: "https://imagewordmatch.firebaseio.com",
+    projectId: "imagewordmatch",
+    storageBucket: "imagewordmatch.appspot.com",
+    messagingSenderId: "621229379920"
+ };
+
+firebase.initializeApp(config);
+
+var database = firebase.database();
+
+var player1Data = null;
+var player2Data = null;
+
+var player1Exists = false;
+var player2Exists = false;
+
+var player1Ref = database.ref('/player1');
+var player2Ref = database.ref('/player2');
+
+
+	$('#submit-displayName').click(function (event) {
+		event.preventDefault();
+
+		displayName = $('#user-displayName').val();
+		console.log(displayName);
+	})
+
+	database.ref().on('value', function (snapshot) {
+		console.log(snapshot.val());
+
+		player1Ref.set({
+			player1Data
+		})
+
+		player2Ref.set({
+			player2Data
+		})
+	})
+
+	// creating game-rooms with max occupancy of 2
+	// when player enters page:
+		// create child node in game-rooms folder firebase
+			// games will be stored here
+			// children of this node will have creator, joiner, and status properties
+				// if room is full, start the game
+
+	var gameRoomRef = database.ref('/game-rooms');
+
+
+	// ---------------- keeping track of connections ----------
+
+	// connectionsRef references a specific location in our database.
+	// All of our connections will be stored in this directory.
+	var connectionsRef = database.ref("/connections");
+
+	// '.info/connected' is a special location provided by Firebase that is updated
+	// every time the client's connection state changes.
+	// '.info/connected' is a boolean value, true if the client is connected and false if they are not.
+	var connectedRef = database.ref(".info/connected");
+
+	// When the client's connection state changes...
+	connectedRef.on("value", function(snap) {
+
+	  // If they are connected..
+	  if (snap.val()) {
+	    // Add user to the connections list.
+	    var con = connectionsRef.push(true);
+	    // Remove user from the connection list when they disconnect.
+	    con.onDisconnect().remove();
+	    console.log(con.key);
+	    userCon = con.key;
+
+	    database.ref("/connections/" + userCon).set(displayName);
+	  } // end of "if (snap.val) statement"
+	}); //end of snap
+
+	// When first loaded or when the connections list changes...
+	connectionsRef.on("value", function(snap) {
+	  // The number of online users is the number of children in the connections list.
+
+	  currentPlayers = snap.numChildren();
+	  console.log('current players: ' + currentPlayers);
+	});
+
+// =============================================================================
+
+
+
+
+
+
+
+
+
 
 
 
